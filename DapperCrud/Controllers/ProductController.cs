d6f3f102016e286a5147f602a996e7997693e8f4
Çağrı Uğurel <cugurel7@gmail.com>
2022-07-10 00:05:39 +0300

    initial commit

---
+++ DapperCrud/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using DapperCrud.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public ProductController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAllProduct()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("NortwindConn"));
+            var products = await connection.QueryAsync<Product>("Select * from Products");
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<List<Product>>> GetAllProduct(int productId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("NortwindConn"));
+            var products = await connection.QueryFirstAsync<Product>("Select * from Products where ProductID = @Id",
+                new {Id = productId});
+            return Ok(products);
+        }
+    }
+}
---
+++ DapperCrud/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperCrud.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
